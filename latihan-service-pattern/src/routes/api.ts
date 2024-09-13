6bd531808ff6a8bf8f735e13b487193576b62518
rezkyyustisio <rezky.yustisio@gmail.com>
2024-09-13 10:56:38 +0700

    tugas-jwt

--- latihan-service-pattern/src/routes/api.ts
+++ latihan-service-pattern/src/routes/api.ts
@@ -5,7 +5,16 @@ import uploadController from "../controllers/upload.controller";
 import productsController from "../controllers/products.controller";
 import categoriesController from "../controllers/categories.controller";
 
+import authController from "../controllers/auth.controller";
+import authMiddleware from "../middlewares/auth.middleware";
+
 const router = express.Router();
+
+// Auth
+router.post("/auth/login", authController.login);
+router.post("/auth/register", authController.register);
+router.post("/auth/me", authMiddleware, authController.me);
+// router.put("/auth/update-profile", authMiddleware, authController.updateProfile);
 
 // CRUD Categories
 router.get("/categories", categoriesController.findAll);
--- latihan-service-pattern/src/utils/env.ts
+++ latihan-service-pattern/src/utils/env.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+export const SECRET: string = process.env.SECRET || "secret";
+
 export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "";
 export const CLOUDINARY_API_SECRET: string =
   process.env.CLOUDINARY_API_SECRET || "";
